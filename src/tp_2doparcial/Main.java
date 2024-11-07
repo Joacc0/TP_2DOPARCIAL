fea3d8a17bcbd757c6a9f6b807a1ccb259f32f7e
Joaquin Lucero <joaquinlucero194@gmail.com>
2024-11-07 15:39:15 -0300

    algunas correciones

--- src/tp_2doparcial/Main.java
+++ src/tp_2doparcial/Main.java
@@ -1,16 +1,12 @@
 package tp_2doparcial;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
         
         String[] arbol ;
-         
-         Scanner leer = new Scanner(System.in);
         
-        Pregunta preg = new Pregunta(22);
+        Pregunta preg = new Pregunta(23);
         Juego comenzar = new Juego();
         
         //Cargamos el arbol con las preguntas.
